23668b2eb8f6450b567f87eb0bd9cc14b698f26d
Dineshkumar S <113044062+dineshsundar30@users.noreply.github.com>
2025-05-29 18:39:23 +0530

    Update basics3.js

--- basics3.js
+++ basics3.js
@@ -67,6 +67,9 @@ var scores1 = [12,13,14,16]
 
 let sumValue=scores1.filter(score=>score%2==0).map(score=>score*3).reduce((sum,val)=>sum+val,0)
 console.log(sumValue)
+
+-------------------------------------------------------------------------------------------------------------------------------------------------
+                                          //  sorting
 let fruits =["banana","mango","pomegrante","apple"]
 
 console.log(fruits.sort())
